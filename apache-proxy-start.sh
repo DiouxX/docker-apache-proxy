8543674fea2af3e2fc29203a44744f88d5f25f74
DiouxX <github@diouxx.be>
2017-04-27 14:54:39 +0200

    Bug fix#2: docker-compose won't start twice: httpd (pid 13) already running

--- apache-proxy-start.sh
+++ apache-proxy-start.sh
@@ -6,5 +6,8 @@ cp /opt/proxy-conf/*.conf /etc/apache2/sites-available/
 #List site and enable
 ls /etc/apache2/sites-available/ -1A | a2ensite *.conf
 
+# Apache gets grumpy about PID files pre-existing
+rm -f /var/run/apache2/apache2.pid
+
 #Launch Apache on Foreground
 /usr/sbin/apache2ctl -D FOREGROUND
